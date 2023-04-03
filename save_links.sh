b2283168b05635f096068c412bd74048e9538a46
Vadim Caen <vadimcaen@gmail.com>
2023-04-03 21:27:18 +0200

    Link save script upgrade

--- save_links.sh
+++ save_links.sh
@@ -1,15 +1,29 @@
 #!/bin/zsh
-# set -x
-TARGET_FILE=".dotfiles/setup_links.sh"
+#set -x
+TARGET_FILE="$HOME/.dotfiles/setup_links.sh"
 
-links=$(find ~ -maxdepth 4 -type l -printf "mkdir -p %h && ln -is %l %p\n"\
-    | grep dotfiles\
-    | sed -s "s#$HOME#~#gi;s#\(?= \)[/~]?dotfiles/#$DIR/#gi;s/mkdir -p ~ && //gi"\
-    | sort -t' ' -k 1r,3)
+# Find all symlink in home dir, skip the dotfiles dir,  print:  (containing dir %h) (link_target $l) (link_name %p)
+links=$(find ~ -maxdepth 4 -name '*dotfiles*' -prune -o -type l -printf "%h %l %p\n")
 
-echo $links
+# Only keep the links pointing to a dotfiles dir
+links=$(echo $links | grep dotfiles)
+
+# Replace ~ with $HOME
+links=$(echo $links | sed -s "s#$HOME#~#gi")
+
+# Sort and ensure unicity
+links=$(echo $links  | sort -t' ' -k 1r,3 | uniq)
+
+# Create the mkdir command for all dir except the home directory (no need to build ourself a home)
+dir=$(echo $links | awk '{print$1}' | grep -vE "^~$" | uniq | sort | awk '$0="mkdir -p "$0')
+
+# Create the ln commands for all links
+ln_commands=$(echo $links | awk '{print "ln -is "$2 " " $3}' | uniq | sort)
+
+out="# Directories:\n$dir\n# Links:\n$ln_commands"
+echo $out
 if read -q "choice?Save to $TARGET_FILE? "; then
-    echo "$links" > $TARGET_FILE;
+    echo "$out" > $TARGET_FILE;
 else
     echo "Not saved"
 fi
--- setup_links.sh
+++ setup_links.sh
@@ -1,27 +1,33 @@
-mkdir -p ~/.dotfiles/bin && ln -is bluetooth/bose_connect ~/.dotfiles/bin/bose_connect
-mkdir -p ~/.dotfiles/android && ln -is ../corp/adebug ~/.dotfiles/android/adebug
-mkdir -p ~/.dotfiles/android && ln -is ../corp/wm ~/.dotfiles/android/wm
-mkdir -p ~/.oh-my-zsh/custom/themes && ln -is ~/.dotfiles/shell/vadim.zsh-theme ~/.oh-my-zsh/custom/themes/vadim.zsh-theme
-mkdir -p ~/.config/kitty && ln -is ~/.dotfiles/kitty/colors.conf ~/.config/kitty/colors.conf
-mkdir -p ~/.config/kitty && ln -is ~/.dotfiles/kitty/diff.conf ~/.config/kitty/diff.conf
-mkdir -p ~/.config/kitty && ln -is ~/.dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
-mkdir -p ~/.config/i3 && ln -is ~/.dotfiles/i3/config ~/.config/i3/config
-mkdir -p ~/.config/i3 && ln -is ~/.dotfiles/i3/i3status ~/.config/i3/i3status
-mkdir -p ~/.config && ln -is ~/.dotfiles/config/mpv ~/.config/mpv
-mkdir -p ~/.config && ln -is ~/.dotfiles/picom.conf ~/.config/picom.conf
-mkdir -p ~/.config && ln -is ~/.dotfiles/rofi ~/.config/rofi
-mkdir -p ~/bin && ln -is ~/.dotfiles/bin/aanim ~/bin/aanim
-mkdir -p ~/bin && ln -is ~/.dotfiles/bin/dc ~/bin/dc
-mkdir -p ~/bin && ln -is ~/.dotfiles/bluetooth/bose_connect ~/bin/boseconnect
-mkdir -p ~/bin && ln -is ~/.dotfiles/corp/ag ~/bin/ag
-ln -is ~/.dotfiles/shell/.zshrc ~/.zshrc
-ln -is ~/.dotfiles/shell/.tmux.conf ~/.tmux.conf
-ln -is ~/.dotfiles/shell/.bashrc ~/.bashrc
-ln -is ~/.dotfiles/shell/.bash_profile ~/.bash_profile
-ln -is ~/.dotfiles/shell/.bash_aliases ~/.bash_aliases
-ln -is ~/.dotfiles/mygitignore.txt ~/.personal_gitignore
-ln -is ~/.dotfiles/i3/dunstrc ~/.dunstrc
-ln -is ~/.dotfiles/i3/.conkyrc ~/.conkyrc
-ln -is ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -is ~/.dotfiles/corp/xsecurelockrc ~/.xsecurelockrc
+# Directories:
+mkdir -p ~/bin
+mkdir -p ~/.config
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/kitty
+mkdir -p ~/.oh-my-zsh/custom/themes
+# Links:
+ln -is ~/.dotfiles/bin/aanim ~/bin/aanim
+ln -is ~/.dotfiles/bin/dc ~/bin/dc
+ln -is ~/.dotfiles/bluetooth/bose_connect ~/bin/boseconnect
+ln -is ~/.dotfiles/config/mpv ~/.config/mpv
+ln -is ~/.dotfiles/corp/ag ~/bin/ag
 ln -is ~/.dotfiles/corp/android_env ~/.android_env
+ln -is ~/.dotfiles/corp/wm ~/bin/wm
+ln -is ~/.dotfiles/corp/xsecurelockrc ~/.xsecurelockrc
+ln -is ~/.dotfiles/git/.gitconfig ~/.gitconfig
+ln -is ~/.dotfiles/i3/config ~/.config/i3/config
+ln -is ~/.dotfiles/i3/.conkyrc ~/.conkyrc
+ln -is ~/.dotfiles/i3/dunstrc ~/.dunstrc
+ln -is ~/.dotfiles/i3/i3status ~/.config/i3/i3status
+ln -is ~/.dotfiles/kitty/colors.conf ~/.config/kitty/colors.conf
+ln -is ~/.dotfiles/kitty/diff.conf ~/.config/kitty/diff.conf
+ln -is ~/.dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -is ~/.dotfiles/mygitignore.txt ~/.personal_gitignore
+ln -is ~/.dotfiles/picom.conf ~/.config/picom.conf
+ln -is ~/.dotfiles/rofi ~/.config/rofi
+ln -is ~/.dotfiles/shell/.bash_aliases ~/.bash_aliases
+ln -is ~/.dotfiles/shell/.bash_profile ~/.bash_profile
+ln -is ~/.dotfiles/shell/.bashrc ~/.bashrc
+ln -is ~/.dotfiles/shell/.profile ~/.profile.bak
+ln -is ~/.dotfiles/shell/.tmux.conf ~/.tmux.conf
+ln -is ~/.dotfiles/shell/vadim.zsh-theme ~/.oh-my-zsh/custom/themes/vadim.zsh-theme
+ln -is ~/.dotfiles/shell/.zshrc ~/.zshrc
